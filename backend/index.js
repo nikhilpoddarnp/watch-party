7bc68792e6a828b732035856cfcc16fc30c6020c
Nikhil <nikhlrajkumarjee3@gmail.com>
2026-07-07 00:50:41 +0530

    ready to deploy

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
