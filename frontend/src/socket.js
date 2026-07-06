6e3f3eaf921fdb9d8dca54e0b6ddee4ad5ad0573
Nikhil <nikhlrajkumarjee3@gmail.com>
2026-07-07 01:02:08 +0530

    add rander backend url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:5000", {
+const socket = io(import.meta.env.VITE_BACKEND_URL || "https://watch-party-j0jw.onrender.com", {
   autoConnect: false, 
 });
 
